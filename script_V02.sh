d1a6723d024d7b5695cc977aaa9e9608831aa81f
AliceDanckaers <alice.danckaers@gmail.com>
2016-01-19 10:15:48 +0100

    starting v0.2

---
+++ script_V02.sh
@@ -0,0 +1,49 @@
+#! /bin/sh
+
+#preparation to blink
+echo 21 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio21/direction
+
+# check loop
+while [ true ]
+do
+
+# blink slowly
+v=1
+count=6
+for i in `seq 1 $count`
+do
+echo $v > /sys/class/gpio/gpio21/value
+usleep 500000
+
+# key detection
+if [ -b "/dev/sda1" ]; then
+
+# mount key
+if ![ -d "/mnt/sda1"]; then mkdir /mnt/sda1; fi
+mount /dev/sda1 /mnt/sda1 
+echo "USB Key is ready to be used"
+
+# blink rapidly
+v=1
+count=6
+for i in `seq 1 $count`
+do
+echo $v > /sys/class/gpio/gpio21/value
+usleep 100000
+
+# unmount key
+umount /dev/sda1
+echo "You can unplug your USB key safely"
+
+# change value of v
+if [ $v = 1 ]; then
+v=0
+else
+v=1
+fi
+
+done
+fi
+sleep 5
+done
