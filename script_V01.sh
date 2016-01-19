4be88e771891ba6b031c176570733d5c0ea83f94
AliceDanckaers <alice.danckaers@gmail.com>
2016-01-19 09:55:40 +0100

    script v0.1 modified

--- script_V01.sh
+++ script_V01.sh
@@ -3,29 +3,29 @@
 #preparation to blink
 echo 21 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio21/direction
-echo "phase1 ok"
+
 # check loop
 while [ true ]
 do
-echo "phase2 ok"
+
 # key detection
 if [ -b "/dev/sda1" ]; then
-echo "phase3 ok"
+
 # mount key
 mkdir /mnt/sda1
 mount /dev/sda1 /mnt/sda1 
-echo "phase4 ok"
+echo "USB Key is ready to be used"
 # unmount key
 umount /dev/sda1
-echo "phase5 ok"
+echo "You can unplug your USB key safely"
 # blink
 v=1
-count=3
+count=6
 for i in `seq 1 $count`
 do
 echo $v > /sys/class/gpio/gpio21/value
-sleep 1
-echo "phase6 ok"
+sleep 0.5
+
 # change value of v
 if [ $v = 1 ]; then
 v=0
